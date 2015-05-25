74f5d3b4431b19d26a9e3f87bde6c1ca2880f493
Leonid Shagabutdinov <leonid@idfly.ru>
2015-05-25 13:15:07 +0600

    views added

---
+++ views/admin.php
@@ -0,0 +1,15 @@
+<?php
+
+use yii\helpers\Html;
+
+?>
+
+<section class="panel">
+    <div class="panel-body">
+        <ul>
+            <li>
+
+            </li>
+        </ul>
+    </div>
+</section>
